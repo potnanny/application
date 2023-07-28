fc168a2c342a4748169c78f397aca93528de744b
Tiny Contest Winners <tinycontestwinners@gmail.com>
2023-07-28 00:22:46 -0400

    update installer script, and turned on ssl/https

--- install.bash
+++ install.bash
@@ -12,34 +12,47 @@
 # version 1.0  02/2023
 #
 
-echo "POTNANNY INSTALLER"
-echo "------------------"
+echo ""
+echo "=============================="
+echo "POTNANNY INSTALLER        v1.0"
+echo "=============================="
+echo ""
 
 echo "Installing requirements..."
-sudo apt update -y
+echo "------------------------------"
+sudo apt-get update -y
+sudo apt-get install build-essential libssl-dev python3-dev python3-pip python3-venv sqlite3 git ufw nginx -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
-sudo apt install build-essential libssl-dev python3-dev python3-pip sqlite3 git ufw nginx -y
+sudo pip3 install --upgrade pip
 
 
 echo "Creating groups..."
+echo "------------------------------"
 sudo groupadd -f potnanny
 sudo usermod -G potnanny -a $USER
 sudo usermod -G bluetooth -a $USER
 
 
+echo "Creating virtualenv..."
+echo "------------------------------"
+cd $HOME
+python3 -m venv venv
+
+
 echo "Creating user directories..."
+echo "------------------------------"
 mkdir $HOME/potnanny
 
 
 echo "Cloning plugin repository..."
+echo "------------------------------"
 cd $HOME/potnanny
 git clone https://github.com/potnanny/plugins.git
 
 
 echo "Installing application..."
-sudo pip3 install --upgrade pip
-sudo pip3 install potnanny
+echo "------------------------------"
+bash -c "source '$HOME/.cargo/env'; source $HOME/venv/bin/activate; pip3 install potnanny;"
 
 
 # create local secret key
@@ -53,11 +66,12 @@ fi
 crontab -l | grep potnanny
 if [ $? -eq 0 ]
 then
-    (crontab -l; echo '@reboot * * * * source $HOME/.profile; potnanny start 2>&1') | crontab
+    (crontab -l; echo '@reboot * * * * source $HOME/.profile; source $HOME/venv/bin/activate; potnanny start 2>&1') | crontab
 fi
 
 
 echo "Generating self-signed certificate for the web server..."
+echo "------------------------------"
 sudo mkdir /etc/ssl/potnanny
 sudo chmod 750 /etc/ssl/potnanny
 sudo chgrp potnanny /etc/ssl/potnanny
@@ -69,6 +83,7 @@ sudo chmod 640 /etc/ssl/potnanny/private.key
 
 
 echo "Configuring firewall..."
+echo "------------------------------"
 sudo ufw allow 22
 sudo ufw allow 80
 sudo ufw allow 8080
@@ -78,6 +93,7 @@ sudo ufw enable
 
 
 echo "Configuring NGINX proxy..."
+echo "------------------------------"
 printf "user www-data;\nworker_processes auto;\npid /run/nginx.pid;\ninclude /etc/nginx/modules-enabled/*.conf;\n\nevents {\n\tworker_connections 768;\n}\n\nhttp {\n\tserver {\n\t\tlisten 80\tdefault;\n\t\treturn 301\thttps://\$host\$request_uri;\n\t}\n\n\tserver {\n\t\tlisten\t443 ssl default_server;\n\t\tlisten\t[::]:443 ssl default_server;\n\t\tserver_name\tpotnanny;\n\t\tclient_max_body_size\t200M;\n\t\tssl_certificate\t\t/etc/ssl/potnanny/certificate.crt;\n\t\tssl_certificate_key\t\t/etc/ssl/potnanny/private.key;\n\t\tlocation / {\n\t\t\tproxy_pass\t\thttp://potnanny:8443;\n\t\t\tproxy_set_header\t\tHost \$host;\n\t\t}\n\t}\n}\n" | sudo tee /etc/nginx/nginx.conf >/dev/null
 sudo service nginx restart
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name='potnanny',
-    version='0.1.0',
+    version='0.1.1',
     python_requires=">=3.9",
     packages=setuptools.find_packages(),
     include_package_data=True,
